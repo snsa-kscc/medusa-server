777e984a786ee35467c64664218ee62ec9339091
Sinisa Koscec <sinisa@dvasadva.com>
2024-03-20 19:49:41 +0100

    add customer to customer group

--- src/api/store/auth/route.ts
+++ src/api/store/auth/route.ts
@@ -1,38 +1,69 @@
 import { MedusaRequest, MedusaResponse } from "@medusajs/medusa";
 import { objectToAuthDataMap, AuthDataValidator } from "@telegram-auth/server";
-
+import TelegramBot from "node-telegram-bot-api";
 import jwt from "jsonwebtoken";
 
+const botToken = process.env.BOT_TOKEN;
+const telegramGroups = process.env.TELEGRAM_GROUPS;
+const bot = new TelegramBot(botToken, { polling: false });
+const validator = new AuthDataValidator({
+  botToken,
+});
+
 export async function POST(req: MedusaRequest, res: MedusaResponse) {
-  const validator = new AuthDataValidator({
-    botToken: process.env.BOT_TOKEN,
-  });
+  try {
+    const data = await req.body;
+    const dataObj = objectToAuthDataMap(data);
+    const telegramUser = await validator.validate(dataObj);
 
-  const data = await req.body;
-  const dataObj = objectToAuthDataMap(data);
-  const telegramUser = await validator.validate(dataObj);
+    const manager = req.scope.resolve("manager");
+    const customerService = req.scope.resolve("customerService");
+    const customerGroupService = req.scope.resolve("customerGroupService");
 
-  const manager = req.scope.resolve("manager");
-  const customerService = req.scope.resolve("customerService");
-  const customerGroupService = req.scope.resolve("customerGroupService");
+    const result1 = await customerGroupService.retrieve("cgrp_01HS6TK3W02RHWXVPSFMNS2DNS", { relations: ["customers"] });
 
-  const result1 = await customerGroupService.retrieve("cgrp_01HS6TK3W02RHWXVPSFMNS2DNS", { relations: ["customers"] });
-  const result2 = await customerService.retrieve("cus_01HS73G7RM7S6188808B213NBB", { relations: ["groups"] });
+    const telegramArray = telegramGroups.split(",").map((group) => group.trim());
 
-  let customer = await customerService.retrieveByPhone(telegramUser.id.toString()).catch(() => null);
+    const statuses = {
+      creator: true,
+      administrator: true,
+      member: true,
+      restricted: true,
+    };
 
-  if (!customer) {
-    customer = await customerService.withTransaction(manager).create({
-      email: `${telegramUser.username}@telegram.telegramUser`,
-      phone: telegramUser.id.toString(),
-      first_name: telegramUser.first_name,
-      last_name: telegramUser.last_name,
-      has_account: true,
-    });
+    let customer = null;
+    for (const group of telegramArray) {
+      const { title } = await bot.getChat(group);
+
+      const result = await bot.getChatMember(group, telegramUser.id).catch(() => null);
+
+      if (result && statuses[result.status]) {
+        // TODO: suboptimal code, hits the database multiple times
+        customer = await customerService.retrieveByPhone(telegramUser.id.toString()).catch(() => null);
+
+        if (!customer) {
+          customer = await customerService.withTransaction(manager).create({
+            email: `${telegramUser.username}@telegram.telegramUser`,
+            phone: telegramUser.id.toString(),
+            first_name: telegramUser.first_name,
+            last_name: telegramUser.last_name,
+            has_account: true,
+          });
+        }
+        const { groups } = await customerService.retrieve(customer.id, { relations: ["groups"] });
+        const groupIdArray = await customerGroupService.list({ name: title }, { select: ["id"] });
+        groups.every((group) => group.name !== title) && (await customerGroupService.addCustomers(groupIdArray[0].id, [customer.id]));
+      }
+    }
+
+    const { projectConfig } = req.scope.resolve("configModule");
+    if (customer) {
+      req.session.jwt_store = jwt.sign({ customer_id: customer.id, domain: "store" }, projectConfig.jwt_secret!, { expiresIn: "30d" });
+    }
+
+    //TODO: suboptimal code - unauthenticated route
+    return res.status(200).json({ token: req.session.jwt_store });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
-
-  const { projectConfig } = req.scope.resolve("configModule");
-  req.session.jwt_store = jwt.sign({ customer_id: customer.id, domain: "store" }, projectConfig.jwt_secret!, { expiresIn: "30d" });
-
-  return res.status(200).json({ token: req.session.jwt_store });
 }
--- src/api/store/bot/route.ts
+++ src/api/store/bot/route.ts
@@ -1,19 +1,41 @@
 import { MedusaRequest, MedusaResponse } from "@medusajs/medusa";
 import TelegramBot from "node-telegram-bot-api";
 
+const botToken = process.env.BOT_TOKEN;
+const bot = new TelegramBot(botToken, { polling: true });
+
 export async function POST(req: MedusaRequest, res: MedusaResponse) {
-  const botToken = process.env.BOT_TOKEN;
-  const bot = new TelegramBot(botToken, { polling: true });
   const vipChatId = -4103272837;
   const plebsChatId = -4175941182;
 
   const telegramGroups = process.env.TELEGRAM_GROUPS;
 
-  for (const group of telegramGroups.split(",")) {
-    const result = await bot.getChatMember(group.trim(), 7127931618).catch(() => null);
-    if (result && (result.status === "creator" || result.status === "administrator" || result.status === "member")) {
-      console.log(group, result.status);
-      break;
+  // for (const group of telegramGroups.split(",")) {
+  //   const result = await bot.getChatMember(group.trim(), 5897870773).catch(() => null);
+  //   if (result && (result.status === "creator" || result.status === "administrator" || result.status === "member" || result.status === "restricted")) {
+  //     console.log(group, result.status);
+  //     break;
+  //   }
+  // }
+
+  const groups = telegramGroups.split(",").map((group) => group.trim());
+
+  const statuses = {
+    creator: true,
+    administrator: true,
+    member: true,
+    restricted: true,
+  };
+
+  for (const group of groups) {
+    try {
+      const result = await bot.getChatMember(group, 5897870773);
+      if (statuses[result.status]) {
+        console.log(group, result.status);
+        break;
+      }
+    } catch (error) {
+      console.log(`Error getting chat member for group ${group}`);
     }
   }
 
