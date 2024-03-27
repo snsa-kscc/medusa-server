3d1c7f2726bca42ab40a08df0e3a2cb8912ae57e
Sinisa Koscec <sinisa@dvasadva.com>
2024-03-27 14:02:18 +0100

    add telegramAuthService logic

--- src/api/store/auth/route.ts
+++ src/api/store/auth/route.ts
@@ -10,72 +10,81 @@ class CustomerNotAuthorizedError extends Error {
   }
 }
 
-const botToken = process.env.BOT_TOKEN;
-const bot = new TelegramBot(botToken, { polling: false });
-const validator = new AuthDataValidator({
-  botToken,
-});
+const botToken: any = process.env.BOT_TOKEN;
+// const bot = new TelegramBot(botToken, { polling: false });
+// const validator = new AuthDataValidator({
+//   botToken,
+// });
 
-const statuses = {
-  creator: true,
-  administrator: true,
-  member: true,
-  restricted: true,
-};
-let customer = null;
+// const statuses = {
+//   creator: true,
+//   administrator: true,
+//   member: true,
+//   restricted: true,
+// };
+// let customer = null;
 
 export async function POST(req: MedusaRequest, res: MedusaResponse) {
   try {
     const data = await req.body;
-    const dataObj = objectToAuthDataMap(data);
-    const telegramUser = await validator.validate(dataObj);
-    const manager = req.scope.resolve("manager");
-    const customerService = req.scope.resolve("customerService");
-    const customerGroupService = req.scope.resolve("customerGroupService");
+    // const dataObj = objectToAuthDataMap(data);
+    // const telegramUser = await validator.validate(dataObj);
+    // const manager = req.scope.resolve("manager");
+    // const customerService = req.scope.resolve("customerService");
+    // const customerGroupService = req.scope.resolve("customerGroupService");
 
-    const customerGroups = await customerGroupService.list();
+    // const customerGroups = await customerGroupService.list();
 
-    const customerGroupsMap = customerGroups.reduce((acc, group) => {
-      acc[group.name] = group.id;
-      return acc;
-    }, {});
-    const telegramGroups = customerGroups.reduce((acc, customerGroup) => {
-      if (customerGroup.metadata && customerGroup.metadata.telegram_group) {
-        acc.push(customerGroup.metadata.telegram_group);
-      }
-      return acc;
-    }, []);
+    // const customerGroupsMap = customerGroups.reduce((acc, group) => {
+    //   acc[group.name] = group.id;
+    //   return acc;
+    // }, {});
+    // const telegramGroups = customerGroups.reduce((acc, customerGroup) => {
+    //   if (customerGroup.metadata && customerGroup.metadata.telegram_group) {
+    //     acc.push(customerGroup.metadata.telegram_group);
+    //   }
+    //   return acc;
+    // }, []);
 
-    for (const telegramGroup of telegramGroups) {
-      const { title } = await bot.getChat(telegramGroup);
-      const chatMember = await bot.getChatMember(telegramGroup, telegramUser.id).catch(() => null);
+    // for (const telegramGroup of telegramGroups) {
+    //   const { title } = await bot.getChat(telegramGroup);
+    //   const chatMember = await bot.getChatMember(telegramGroup, telegramUser.id).catch(() => null);
 
-      if (chatMember && statuses[chatMember.status]) {
-        customer = await customerService.retrieveByPhone(telegramUser.id.toString(), { relations: ["groups"] }).catch(() => null);
+    //   if (chatMember && statuses[chatMember.status]) {
+    //     customer = await customerService.retrieveByPhone(telegramUser.id.toString(), { relations: ["groups"] }).catch(() => null);
 
-        if (!customer) {
-          customer = await customerService.withTransaction(manager).create({
-            email: `${telegramUser.username}@telegram.telegramUser`,
-            phone: telegramUser.id.toString(),
-            first_name: telegramUser.first_name,
-            last_name: telegramUser.last_name,
-            has_account: true,
-          });
-        }
+    //     if (!customer) {
+    //       customer = await customerService.withTransaction(manager).create({
+    //         email: `${telegramUser.username}@telegram.telegramUser`,
+    //         phone: telegramUser.id.toString(),
+    //         first_name: telegramUser.first_name,
+    //         last_name: telegramUser.last_name,
+    //         has_account: true,
+    //       });
+    //     }
 
-        customer.groups.every((group) => group.name !== title) &&
-          (await customerGroupService.withTransaction(manager).addCustomers(customerGroupsMap[title], [customer.id]));
-      } else if (chatMember && !statuses[chatMember.status]) {
-        const rejectedCustomer = await customerService.retrieveByPhone(telegramUser.id.toString(), { relations: ["groups"] });
-        if (rejectedCustomer.groups.find((group) => group.name === title)) {
-          await customerGroupService.withTransaction(manager).removeCustomer(customerGroupsMap[title], [rejectedCustomer.id]);
-        }
-      }
-    }
+    //     customer.groups.every((group) => group.name !== title) &&
+    //       (await customerGroupService.withTransaction(manager).addCustomers(customerGroupsMap[title], [customer.id]));
+    //   } else if (chatMember && !statuses[chatMember.status]) {
+    //     const rejectedCustomer = await customerService.retrieveByPhone(telegramUser.id.toString(), { relations: ["groups"] });
+    //     if (rejectedCustomer.groups.find((group) => group.name === title)) {
+    //       await customerGroupService.withTransaction(manager).removeCustomer(customerGroupsMap[title], [rejectedCustomer.id]);
+    //     }
+    //   }
+    // }
+
+    // if (!customer) {
+    //   throw new CustomerNotAuthorizedError();
+    // }
+
+    const telegramAuthService = req.scope.resolve("telegramAuthService");
+
+    const customer = await telegramAuthService.processTelegramUser(data);
 
     if (!customer) {
       throw new CustomerNotAuthorizedError();
     }
+
     const { projectConfig } = req.scope.resolve("configModule");
     req.session.jwt_store = jwt.sign({ customer_id: customer.id, domain: "store" }, projectConfig.jwt_secret!, { expiresIn: "30d" });
 
--- src/services/telegram-auth.ts
+++ src/services/telegram-auth.ts
@@ -1,25 +1,94 @@
-import { CustomerService, CustomerGroupService, TransactionBaseService } from "@medusajs/medusa";
+import { CustomerService, CustomerGroupService, TransactionBaseService, Customer } from "@medusajs/medusa";
 import { EntityManager } from "typeorm";
 import { objectToAuthDataMap, AuthDataValidator } from "@telegram-auth/server";
-import TelegramBot from "node-telegram-bot-api";
-import jwt from "jsonwebtoken";
+import TelegramBot, { ChatMember } from "node-telegram-bot-api";
+// import jwt from "jsonwebtoken";
+
+// class CustomerNotAuthorizedError extends Error {
+//   constructor() {
+//     super("Customer not authorized.");
+//     this.name = "CustomerNotAuthorizedError";
+//   }
+// }
 
 class TelegramAuthService extends TransactionBaseService {
-  protected manager: EntityManager;
-  protected transactionManager: EntityManager;
+  protected manager_: EntityManager;
+  protected transactionManager_: EntityManager;
+  private botToken_: string;
   private bot_: TelegramBot;
   private validator_: AuthDataValidator;
-  private customerService: CustomerService;
-  private customerGroupService: CustomerGroupService;
+  private customerService_: CustomerService;
+  private customerGroupService_: CustomerGroupService;
 
   constructor(container) {
     super(container);
-    this.customerService = container.customerService;
-    this.customerGroupService = container.customerGroupService;
+    this.botToken_ = process.env.BOT_TOKEN;
+    this.customerService_ = container.customerService;
+    this.customerGroupService_ = container.customerGroupService;
+    this.bot_ = new TelegramBot(this.botToken_, { polling: false });
+    this.validator_ = new AuthDataValidator({ botToken: this.botToken_ });
   }
-  async getCustomerGroups() {
-    const res = await this.customerGroupService.list({}, {});
-    return res;
+
+  async processTelegramUser(data: any) {
+    const statuses = {
+      creator: true,
+      administrator: true,
+      member: true,
+      restricted: true,
+    };
+
+    const dataObj = objectToAuthDataMap(data);
+    const telegramUser = await this.validator_.validate(dataObj);
+    const manager = this.manager_;
+    const customerService = this.customerService_;
+    const customerGroupService = this.customerGroupService_;
+
+    const customerGroups = await customerGroupService.list({}, {});
+
+    const customerGroupsMap = customerGroups.reduce((acc, group) => {
+      acc[group.name] = group.id;
+      return acc;
+    }, {});
+    const telegramGroups: string[] = customerGroups.reduce((acc, customerGroup) => {
+      if (customerGroup.metadata && customerGroup.metadata.telegram_group) {
+        acc.push(customerGroup.metadata.telegram_group);
+      }
+      return acc;
+    }, []);
+
+    let customer: Customer | null = null;
+
+    for (const telegramGroup of telegramGroups) {
+      const { title } = await this.bot_.getChat(telegramGroup);
+      const chatMember: ChatMember | null = await this.bot_.getChatMember(telegramGroup, telegramUser.id).catch(() => null);
+
+      if (chatMember && statuses[chatMember.status]) {
+        customer = await customerService.retrieveByPhone(telegramUser.id.toString(), { relations: ["groups"] }).catch(() => null);
+
+        if (!customer) {
+          customer = await customerService.withTransaction(manager).create({
+            email: `${telegramUser.username}@telegram.telegramUser`,
+            phone: telegramUser.id.toString(),
+            first_name: telegramUser.first_name,
+            last_name: telegramUser.last_name,
+            has_account: true,
+          });
+        }
+
+        if (customer.groups.every((group) => group.name !== title)) {
+          await customerGroupService.withTransaction(manager).addCustomers(customerGroupsMap[title], [customer.id]);
+        }
+      } else if (chatMember && !statuses[chatMember.status]) {
+        const rejectedCustomer = await customerService.retrieveByPhone(telegramUser.id.toString(), { relations: ["groups"] });
+        if (rejectedCustomer.groups.find((group) => group.name === title)) {
+          await customerGroupService.withTransaction(manager).removeCustomer(customerGroupsMap[title], [rejectedCustomer.id]);
+        }
+      }
+    }
+    // if (!customer) {
+    //   throw new CustomerNotAuthorizedError();
+    // }
+    return customer;
   }
 }
 
